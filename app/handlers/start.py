f0062e4d270765ace71772fc7d582bba9d7689b4
Avramradar <avram.aleksey@yandex.com>
2026-08-02 16:25:43 +0300

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -2,12 +2,23 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from app.database.repositories.user_repository import (
+    create_or_update_user,
+)
+from app.database.session import async_session_maker
+
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_handler(message: Message) -> None:
+    async with async_session_maker() as session:
+        await create_or_update_user(
+            session=session,
+            telegram_user=message.from_user,
+        )
+
     await message.answer(
         "<b>MarkaRadar</b>\n\n"
         "Я помогу выбрать продукты по оценкам пользователей, "
