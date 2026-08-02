7c0962899d0e7bd61795b7ed1932b42cb7847d38
Avramradar <avram.aleksey@yandex.com>
2026-08-02 09:50:23 +0300

    Update __init__.py

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -1,1 +1,23 @@
-# Database models package
+from app.database.models.brand import Brand
+from app.database.models.category import Category
+from app.database.models.price import PriceObservation
+from app.database.models.product import Product
+from app.database.models.product_alias import ProductAlias
+from app.database.models.product_relation import ProductRelation
+from app.database.models.rating import Rating
+from app.database.models.review import Review
+from app.database.models.search_history import SearchHistory
+from app.database.models.user import User
+
+__all__ = [
+    "Brand",
+    "Category",
+    "PriceObservation",
+    "Product",
+    "ProductAlias",
+    "ProductRelation",
+    "Rating",
+    "Review",
+    "SearchHistory",
+    "User",
+]
