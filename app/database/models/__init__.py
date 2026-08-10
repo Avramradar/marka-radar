24bae0a3f304ca6bd9d096fd3a54d14bd61b5e63
Avramradar <avram.aleksey@yandex.com>
2026-08-10 07:47:10 +0300

    Update __init__.py

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -5,6 +5,7 @@ from app.database.models.product import Product
 from app.database.models.product_alias import ProductAlias
 from app.database.models.product_family import ProductFamily
 from app.database.models.product_relation import ProductRelation
+from app.database.models.product_source import ProductSource
 from app.database.models.rating import Rating
 from app.database.models.review import Review
 from app.database.models.search_history import SearchHistory
@@ -19,6 +20,7 @@ __all__ = (
     "ProductAlias",
     "ProductFamily",
     "ProductRelation",
+    "ProductSource",
     "Rating",
     "Review",
     "SearchHistory",
